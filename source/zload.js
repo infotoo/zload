e9cec581b42e9d54998aa910229ba7cfc67d7e1f
Ah Bo Lee <ahbolee@users.noreply.github.com>
2017-03-08 11:45:26 +0800

    Version 3.2

--- source/zload.js
+++ source/zload.js
@@ -6,56 +6,56 @@
  */
 (function(){
 
-    ~import 'lazyload.js';
+	~import 'lazyload.js';
 
-    var $m = {
-        events: {},
-        init: function( name, v ){},
-        load: function( name ){},
-        ready: function( a ){},
-        register: function( name, fn ){},
-        trigger: function( name ){}
-    };
+	var $m = {
+		events: {},
+		init: function( name, v ){},
+		load: function( name ){},
+		ready: function( a ){},
+		register: function( name, fn ){},
+		trigger: function( name ){}
+	};
 
-    var $z = function(){
-            $m.ready( arguments );
-        };
-    $z.map = {};
-    $z.version = '3.2';
+	var $z = function(){
+			$m.ready( arguments );
+		};
+	$z.map = {};
+	$z.version = '3.2';
 
-    $m.init = function( name, v ){
-            if( !$z.map[name] ){
-                $z.map[name] = v;
-            }else{
-                for( var i in v ){
-                    $z.map[name][i] = v[i];
-                }
-            }
-        };
+	$m.init = function( name, v ){
+			if( !$z.map[name] ){
+				$z.map[name] = v;
+			}else{
+				for( var i in v ){
+					$z.map[name][i] = v[i];
+				}
+			}
+		};
 
-    $m.load = function( name ){
-            if( !$z.map[name] ){
-                return;
-            }
-            if( $z.map[name].isReady ){
-                $m.trigger( name );
-                return;
-            }
-            if( $z.map[name].isLoad ){
-                return;
-            }
-            if( $z.map[name].load && typeof($z.map[name].load)=='function' ){
-                $z.map[name].load();
-            }else{
-                $z.map[name].isLoad = 1;
+	$m.load = function( name ){
+			if( !$z.map[name] ){
+				return;
+			}
+			if( $z.map[name].isReady ){
+				$m.trigger( name );
+				return;
+			}
+			if( $z.map[name].isLoad ){
+				return;
+			}
+			if( $z.map[name].load && typeof($z.map[name].load)=='function' ){
+				$z.map[name].load();
+			}else{
+				$z.map[name].isLoad = 1;
 
-                var checkCount = ( ($z.map[name].js)? $z.map[name].js.length : 0 ) + ( ($z.map[name].css)? $z.map[name].css.length : 0 ) + ( ($z.map[name].uses)? $z.map[name].uses.length : 0 );
+				var checkCount = ( ($z.map[name].js)? $z.map[name].js.length : 0 ) + ( ($z.map[name].css)? $z.map[name].css.length : 0 ) + ( ($z.map[name].uses)? $z.map[name].uses.length : 0 );
 				var runCount = ($z.map[name].fn)? $z.map[name].fn.length : 0;
 				var runIndex = 0;
 
-                var checkReady = function(){
-                    checkCount -= 1;
-                    if( checkCount <= 0 ){
+				var checkReady = function(){
+					checkCount -= 1;
+					if( checkCount <= 0 ){
 						if( runIndex < runCount ){
 							runIndex += 1;
 							(function( func ){
@@ -71,66 +71,66 @@
 							$z.map[name].isReady = 1;
 							$m.trigger( name );
 						}
-                    }
-                };
+					}
+				};
 
-                if( $z.map[name].uses ){
-                    for( var i = 0; i<$z.map[name].uses.length; i++ ){
-                        $zload( $z.map[name].uses[i], checkReady );
-                    }
-                }
-                //#Fix Bug in Chrome, so call one by one
-                if( $z.map[name].css ){
-                    for( var i = 0; i<$z.map[name].css.length; i++ ){
-                        LazyLoad.css( $z.map[name].css[i], checkReady );
-                    }
-                }
-                //#Fix Bug in Chrome, so call one by one
-                if( $z.map[name].js ){
-                    for( var i = 0; i<$z.map[name].js.length; i++ ){
-                        LazyLoad.js( $z.map[name].js[i], checkReady );
-                    }
-                }
+				if( $z.map[name].uses ){
+					for( var i = 0; i<$z.map[name].uses.length; i++ ){
+						$zload( $z.map[name].uses[i], checkReady );
+					}
+				}
+				//#Fix Bug in Chrome, so call one by one
+				if( $z.map[name].css ){
+					for( var i = 0; i<$z.map[name].css.length; i++ ){
+						LazyLoad.css( $z.map[name].css[i], checkReady );
+					}
+				}
+				//#Fix Bug in Chrome, so call one by one
+				if( $z.map[name].js ){
+					for( var i = 0; i<$z.map[name].js.length; i++ ){
+						LazyLoad.js( $z.map[name].js[i], checkReady );
+					}
+				}
 
-            }
-        };
+			}
+		};
 
-    $m.ready = function( a ){
-            if( typeof(a[0])=='string' ){
-                var name = a[0];
-                if( a.length > 1 ){
-                    for( var i = 1; i < a.length; i++ ){
-                        var v = a[i];
-                        if( typeof(v)=='object' ){
-                            $m.init( name, v );
-                        }
-                        else if( typeof(v)=='function' ){
-                            $m.register( name, v );
-                        }
-                        else if( typeof(v)=='number' ){
-                            setTimeout( function(){ $m.load(name); }, v );
-                        }
-                    }
-                }
-                $m.load( name );
-            }
-        };
+	$m.ready = function( a ){
+			if( typeof(a[0])=='string' ){
+				var name = a[0];
+				if( a.length > 1 ){
+					for( var i = 1; i < a.length; i++ ){
+						var v = a[i];
+						if( typeof(v)=='object' ){
+							$m.init( name, v );
+						}
+						else if( typeof(v)=='function' ){
+							$m.register( name, v );
+						}
+						else if( typeof(v)=='number' ){
+							setTimeout( function(){ $m.load(name); }, v );
+						}
+					}
+				}
+				$m.load( name );
+			}
+		};
 
-    $m.register = function( name, fn ){
-            if( !$m.events[name] ){
-                $m.events[name] = [];
-            }
-            $m.events[name].push( fn );
-        };
+	$m.register = function( name, fn ){
+			if( !$m.events[name] ){
+				$m.events[name] = [];
+			}
+			$m.events[name].push( fn );
+		};
 
-    $m.trigger = function( name ){
-            if( $m.events[name] && $m.events[name].length > 0 ){
-                var fn = $m.events[name].shift();
-                setTimeout(function(){ $m.trigger(name); }, 1 );
-                fn();
-            }
-        };
+	$m.trigger = function( name ){
+			if( $m.events[name] && $m.events[name].length > 0 ){
+				var fn = $m.events[name].shift();
+				setTimeout(function(){ $m.trigger(name); }, 1 );
+				fn();
+			}
+		};
 
-    window.$zload = $z;
+	window.$zload = $z;
 
 })();
